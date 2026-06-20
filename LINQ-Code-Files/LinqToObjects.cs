4029aaa7c05b597ef63e65db6bd456ef86905915
Manish <msajandasani@gmail.com>
2026-06-20 14:16:48 +0530

    feat: added linq to objects file

---
+++ LINQ-Code-Files/LinqToObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpLinqPractice.LINQCodeFiles
+{
+    public static class LinqToObjects
+    {
+        public static void Run(int[] numbers) {
+            IEnumerable<int> evenNumbers = from number in numbers
+                                           where number % 2 == 0
+                                           select number;
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using CSharpLinqPractice.LINQCodeFiles;
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+LinqToObjects.Run(numbers);
+
+Console.ReadKey();
